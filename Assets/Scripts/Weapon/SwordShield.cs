2921ed8085e95a7eb01f38978ab00bc11bfdc47d
Mathieu <mathieu.herranzperez@gmail.com>
2019-09-28 19:43:11 +0200

    Weapon combo

--- Assets/Scripts/Weapon/SwordShield.cs
+++ Assets/Scripts/Weapon/SwordShield.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class SwordShield : MeleeWeapon
 {
+    [SerializeField]
+    private int maxCombo = 3;
+    [SerializeField]
+    private float timeToCombo = 3f;
+
+    // ---- INERN ----
+    private int nbCombo = 0;
+    private float timeLeftToCombo = 0f;
+
+    void Update()
+    {
+        if(timeLeftToCombo > 0f)
+        {
+            timeLeftToCombo -= Time.deltaTime;
+        }
+        else
+        {
+            nbCombo = 0;        // reset the combo
+        }
+    }
 
     public override void ActiveDefensiveColliders()
     {
@@ -30,6 +50,11 @@ public class SwordShield : MeleeWeapon
         if (canAttack)
         {
             animator.SetTrigger("BasicAttack");
+            animator.SetInteger("AttackNum", nbCombo);
+            timeLeftToCombo = timeToCombo;
+
+            nbCombo = nbCombo < maxCombo ? nbCombo + 1 : 0;
+
             canAttack = false;
             StartCoroutine("CountAttackCouldown");
         }
