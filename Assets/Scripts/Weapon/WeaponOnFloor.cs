f4092d6fcc8cc0d9d008d0a0dfc9072f7546a540
Mathieu <mathieu.herranzperez@gmail.com>
2019-12-01 21:18:18 +0100

    Get weapon on floor

---
+++ Assets/Scripts/Weapon/WeaponOnFloor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class WeaponOnFloor : MonoBehaviour
+{
+    [Header("Setup")]
+    [SerializeField]
+    private GameObject WeaponPrefab = default;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<WeaponManager>().ChangeWeapon(WeaponPrefab);
+            Destroy(gameObject);
+        }
+    }
+}
