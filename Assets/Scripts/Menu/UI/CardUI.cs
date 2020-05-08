816f15d615d91c8ab57d6e310592945fffc1dfaa
Mathieu <mathieu.herranzperez@gmail.com>
2020-05-08 22:27:52 +0200

    card on clic

---
+++ Assets/Scripts/Menu/UI/CardUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class CardUI : MonoBehaviour
+{
+    private Button button;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void ChangeListener(Action<GameObject> func)
+    {
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(delegate { func(gameObject); });
+    }
+}
--- Assets/Scripts/Menu/UI/DeckUI.cs
+++ Assets/Scripts/Menu/UI/DeckUI.cs
@@ -62,6 +62,67 @@ public class DeckUI : MonoBehaviour, Observer
         deck.DisplayDeckSelectedInConsole(); //affD
     }
 
+    public void NotifyClicOnCardFromSlot(GameObject go)
+    {
+        // put it in the container
+        go.transform.SetParent(cardsListGOUI.transform);
+        // change the clic behaviour
+        go.GetComponent<CardUI>().ChangeListener(delegate { NotifyClicOnCardFromContainer(go); });
+
+        menuWeaponManager.RemoveWeapon(go.GetComponent<Card>().WeaponPrefab);
+
+        // for all the slots, add the card if not already added or remove it if null
+        for (int i = 0; i < slotList.Length; ++i)
+        {
+            if (slotList[i].cardGO)
+            {
+                deck.SelectCardIfNotSelected(slotList[i].cardGO.GetComponent<Card>(), i);
+            }
+            else
+            {
+                deck.DeselectCard(i);
+            }
+        }
+
+        deck.DisplayDeckSelectedInConsole(); //affD
+    }
+
+    public void NotifyClicOnCardFromContainer(GameObject go)
+    {
+        // check if there is space in selected
+        bool isFull = true;
+        int i = 0;
+        while(i < selectedCardsGOUI.transform.childCount && isFull)
+        {
+            if(selectedCardsGOUI.transform.GetChild(i).childCount == 0)
+                isFull = false;
+            else
+                ++i;
+        }
+
+        if (!isFull)
+        {
+            // put it in the slot
+            go.transform.SetParent(selectedCardsGOUI.transform.GetChild(i).transform);
+            // change the clic behaviour
+            go.GetComponent<CardUI>().ChangeListener(delegate { NotifyClicOnCardFromSlot(go); });
+
+            menuWeaponManager.GiveNewWeapon(go.GetComponent<Card>().WeaponPrefab);
+
+            // for all the slots, add the card if not already added
+            for (int j = 0; j < slotList.Length; ++j)
+            {
+                if (slotList[j].cardGO)
+                {
+                    deck.SelectCardIfNotSelected(slotList[j].cardGO.GetComponent<Card>(), j);
+                }
+            }
+
+            deck.DisplayDeckSelectedInConsole(); //affD
+        }
+
+    }
+
     public void Notify()
     {
         Clear();
@@ -75,6 +136,10 @@ public class DeckUI : MonoBehaviour, Observer
             if (card)
             {
                 Card cardClone = (Card)Instantiate(card, cardSlot.transform);  // instantiate the card in the UI
+                cardClone.gameObject.AddComponent(typeof(CardUI));
+                CardUI cardUI = cardClone.GetComponent<CardUI>();
+                cardUI.ChangeListener(delegate { NotifyClicOnCardFromSlot(cardClone.gameObject); });
+
                 cardClone.gameObject.AddComponent(typeof(DraggableUIObject));
                 cardClone.gameObject.AddComponent(typeof(CanvasGroup));
             }
@@ -85,6 +150,9 @@ public class DeckUI : MonoBehaviour, Observer
         {
             ++j;
             Card cardClone = (Card)Instantiate(c, cardsListGOUI.transform);  // instantiate the card in the UI
+            cardClone.gameObject.AddComponent(typeof(CardUI));
+            CardUI cardUI = cardClone.GetComponent<CardUI>();
+            cardUI.ChangeListener(delegate { NotifyClicOnCardFromContainer(cardClone.gameObject); });
             cardClone.gameObject.AddComponent(typeof(DraggableUIObject));
             cardClone.gameObject.AddComponent(typeof(CanvasGroup));
         }
